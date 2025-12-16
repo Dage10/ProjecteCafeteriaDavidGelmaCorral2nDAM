819f77783aaf4c537b8aa643c4c2499cb7fd8316
Dage10 <davidgelmacorral@gmail.com>
2025-12-16 12:25:03 +0100

    Implementar NavigationDrawer

--- app/src/main/java/com/daviddam/projectecafeteriadavidgelmacorral2ndam/MainActivity.kt
+++ app/src/main/java/com/daviddam/projectecafeteriadavidgelmacorral2ndam/MainActivity.kt
@@ -3,11 +3,15 @@ package com.daviddam.projectecafeteriadavidgelmacorral2ndam
 import android.content.Intent
 import android.os.Bundle
 import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.ActionBarDrawerToggle
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.splashscreen.SplashScreen.Companion.installSplashScreen
+import androidx.drawerlayout.widget.DrawerLayout
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.setupWithNavController
 import com.google.android.material.bottomnavigation.BottomNavigationView
+import com.google.android.material.navigation.NavigationView
 import sharedPreference.SharedPreference
 
 class MainActivity : AppCompatActivity() {
@@ -30,6 +34,20 @@ class MainActivity : AppCompatActivity() {
         val navController = navHostFragment.navController
         val bottomNav = findViewById<BottomNavigationView>(R.id.bottomNavigationView)
         bottomNav.setupWithNavController(navController)
+
+
+        val toolbar = findViewById<Toolbar>(R.id.toolbar)
+        setSupportActionBar(toolbar)
+
+        val drawerLayout = findViewById<DrawerLayout>(R.id.main)
+        val navigationView = findViewById<NavigationView>(R.id.navigationView)
+        val toggle = ActionBarDrawerToggle(
+            this, drawerLayout, toolbar, R.string.drawer_obrir, R.string.drawer_tancar
+        )
+        drawerLayout.addDrawerListener(toggle)
+        toggle.syncState()
+
+        navigationView.setupWithNavController(navController)
 
     }
 }
